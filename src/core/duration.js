6ca20bae2a9e6cc3971a218f351e54445bcc856a
Damon Oehlman <damon.oehlman@sidelab.com>
2012-05-01 12:33:57 +1000

    Added formatting test

--- src/core/duration.js
+++ src/core/duration.js
@@ -41,58 +41,61 @@ Duration.prototype.ticks = function() {
     return (this.days * DAY_SECONDS + this.seconds) * 1000;
 };
     
-Duration.prototype.toString = function(formatter) {
-    var output;
+Duration.prototype.toString = function(format) {
+    var output = [];
     
     // ensure normalized
     this.normalize();
 
     // if the format is not defined, return the simple format
     if (! format) {
-        output = this.days + 'd' + this.seconds + 's';
-    } // if
+        return this.days + 'd' + this.seconds + 's';
+    }
+    else {
+        var hours, minutes;
         
-    /*
-    if (this.days) {
-        output += this.days + 'd';
-    } // if
+        if (this.days) {
+            output.push(this.days + ' days');
+        } // if
     
-    if (this.seconds) {
-        totalSeconds = this.seconds;
+        if (this.seconds) {
+            totalSeconds = this.seconds;
 
-        // if we have hours, then get them
-        if (totalSeconds >= 3600) {
-            hours = ~~(totalSeconds / 3600);
-            totalSeconds = totalSeconds - (hours * 3600);
-        } // if
+            // if we have hours, then get them
+            if (totalSeconds >= 3600) {
+                hours = ~~(totalSeconds / 3600);
+                totalSeconds = totalSeconds - (hours * 3600);
+            } // if
         
-        // if we have minutes then extract those
-        if (totalSeconds >= 60) {
-            minutes = Math.round(totalSeconds / 60);
-            totalSeconds = totalSeconds - (minutes * 60);
-        } // if
+            // if we have minutes then extract those
+            if (totalSeconds >= 60) {
+                minutes = Math.round(totalSeconds / 60);
+                totalSeconds = totalSeconds - (minutes * 60);
+            } // if
         
-        // format the result
-        if (hours) {
-            output = output + hours + 
-                (hours > 1 ? ' hrs ' : ' hr ') + 
-                (minutes ? 
-                    (minutes > 10 ? 
-                        minutes : 
-                        '0' + minutes) + ' min ' 
-                    : '');
-        }
-        else if (minutes) {
-            output = output + minutes + ' min';
-        }
-        else if (totalSeconds > 0) {
-            output = output + 
-                (totalSeconds > 10 ? 
-                    totalSeconds : 
-                    '0' + totalSeconds) + ' sec';
-        } // if..else
-    } // if
-    */
+            // format the result
+            if (hours) {
+                output.push(hours + (hours > 1 ? ' hrs' : ' hr'));
+                output.push(
+                    (minutes ? 
+                        (minutes > 10 ? 
+                            minutes : 
+                            '0' + minutes) + ' min' 
+                        : '')
+                );
+            }
+            else if (minutes) {
+                output.push(minutes + 'min');
+            }
+            else if (totalSeconds > 0) {
+                output.push(
+                    (totalSeconds > 10 ? 
+                        totalSeconds : 
+                        '0' + totalSeconds) + ' sec'
+                );
+            } // if..else
+        } // if
+    }
     
     return output;
 };
---
+++ test/formatting.js
@@ -0,0 +1,35 @@
+var timelord = require('../pkg/cjs/timelord'),
+    expect = require('expect.js');
+
+describe('duration formatting', function() {
+    it('can be initialized with days and seconds as numeric values', function() {
+        var test = timelord('10d5s');
+        
+        expect(test.toString()).to.equal('10d5s');
+    });
+    
+    it('can be initialized with just days', function() {
+        var test = timelord('10d');
+
+        expect(test.toString()).to.equal('10d0s');
+    });
+    
+    it('can be initialized from an existing duration object', function() {
+        var test1 = timelord('10d5s'),
+            test2 = test1.clone();
+            
+        expect(test2.toString()).to.equal('10d5s');
+    });
+    
+    it('can be initialized from a valid object literal', function() {
+        var test = timelord({ d: 10, s: 5 });
+        
+        expect(test.toString()).to.equal('10d5s');
+    });
+    
+    it('can be initialized with many seconds, but be normalized', function() {
+        var test = timelord({ s: 1000000 });
+        
+        expect(test.toString()).to.equal('11d49600s');
+    });
+});
